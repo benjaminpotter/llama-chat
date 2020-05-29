7232af47c90a168d6a10fa8ba3797d4d19a03096
unknown <reactiveneon@gmail.com>
2020-05-29 01:26:41 -0400

    damn thats good.

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const app = express();
 
+const axios = require('axios');
+axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
+axios.defaults.headers.post['Authorization'] = 'k3j5DxNUV9P5njlYxdmhAAceAE2t5mmp';
+
 const PORT = 3000;
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.use(express.static('client'));
 
@@ -19,5 +29,18 @@ const io = require('socket.io')(http);
 io.on('connection', (socket) => {
 
     console.log('socket connected');
+
+    socket.on('enter-request', (data) => {
+        // when a user presses the enter button
+
+        // check validity
+
+        socket.emit('enter-auth-success');
+    });
+
+    socket.on('sent-gif', data => {
+        console.log('hellow?');
+        socket.emit('recieve-gif', {url: data.url});
+    });
 
 });
